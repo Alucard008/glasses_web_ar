2f4fe6ed74be8ff458edbb4c460ee59ca9f9269e
Alucard008 <abdullahmasood163@gmail.com>
2024-08-01 12:03:17 +0500

    Update VTOGlasses.js

--- src/js/demos/VTOGlasses.js
+++ src/js/demos/VTOGlasses.js
@@ -1,30 +1,14 @@
 import React, { useEffect, useRef, useState, Suspense } from 'react';
 import { Canvas, useFrame, useThree, useLoader } from '@react-three/fiber';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
-
-// import GLTF loader - originally in examples/jsm/loaders/
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
-// import components:
 import BackButton from '../components/BackButton.js';
 import VTOButton from '../components/VTOButton.js';
-
-// import neural network model:
 import NN from '../contrib/WebARRocksFace/neuralNets/NN_GLASSES_9.json';
-
-// import WebARRocksMirror, a helper
-// This helper is not minified, feel free to customize it (and submit pull requests bro):
 import mirrorHelper from '../contrib/WebARRocksFace/helpers/WebARRocksMirror.js';
-
-// ASSETS:
-// import 3D models of sunglasses
 import GLTFModel1 from '../../assets/VTOGlasses/models3D/glasses2.glb';
 import GLTFModel2 from '../../assets/VTOGlasses/models3D/Red Glasses.glb';
-
-// import occluder
 import GLTFOccluderModel from '../../assets/VTOGlasses/models3D/occluder1.glb';
-
-// import envMap:
 import envMap from '../../assets/VTOGlasses/envmaps/venice_sunset_1k.hdr';
 
 let _threeFiber = null;
@@ -33,8 +17,6 @@ const get_pauseButtonText = (isPaused) => {
   return isPaused ? 'Resume' : 'Pause';
 };
 
-// fake component, display nothing
-// just used to get the Camera and the renderer used by React-fiber:
 const ThreeGrabber = (props) => {
   const threeFiber = useThree();
   _threeFiber = threeFiber;
@@ -46,12 +28,9 @@ const ThreeGrabber = (props) => {
 };
 
 const compute_sizing = () => {
-  // compute  size of the canvas:
   const height = window.innerHeight;
   const wWidth = window.innerWidth;
   const width = Math.min(wWidth, height);
-
-  // compute position of the canvas:
   const top = 0;
   const left = (wWidth - width) / 2;
   return { width, height, top, left };
@@ -75,15 +54,12 @@ const VTOModelContainer = (props) => {
       threeObject3D,
       props.faceIndex
     );
-    //return mirrorHelper.clean;
   }, [props.GLTFModel, props.sizing]);
 
-  // import main model:
   const gltf = useLoader(GLTFLoader, props.GLTFModel);
   const model = gltf.scene.clone();
 
-  // import and create occluder:
-  const isDebugOccluder = false; // true to debug the occluder
+  const isDebugOccluder = false;
   const gltfOccluder = useLoader(GLTFLoader, props.GLTFOccluderModel);
   const occluderModel = gltfOccluder.scene.clone();
   const occluderMesh = mirrorHelper.create_occluderMesh(
@@ -115,42 +91,34 @@ const VTOGlasses = (props) => {
   const PI = 3.1415;
   const scale = 100;
 
-  // state:
   const [sizing, setSizing] = useState(compute_sizing());
   const [model, setModel] = useState(GLTFModel1);
   const [isInitialized] = useState(true);
+  const [choice, setChoice] = useState(false);
+  const [modelChoice, setModelChoice] = useState('');
 
-  // refs:
   const togglePauseRef = useRef();
   const canvasFaceRef = useRef();
 
-  // misc private vars:
   const _settings = {
     glassesBranches: {
-      // Branch fading parameters (branch become transparent near the ears)
-      fadingZ: -0.9, // where to start branch fading. - -> to the back
-      fadingTransition: 0.6, // 0 -> hard transition
-
-      // Branch bending (glasses branches are always bent to slightly tighten the head):
-      bendingAngle: 5, //in degrees. 0 -> no bending
-      bendingZ: 0, //start brench bending at this position. - -> to the back
+      fadingZ: -0.9,
+      fadingTransition: 0.6,
+      bendingAngle: 5,
+      bendingZ: 0,
     },
-
     lighting: {
       envMap,
-      pointLightIntensity: 0.8, // intensity of the point light. Set to 0 to disable
-      pointLightY: 200, // larger -> move the pointLight to the top
-      hemiLightIntensity: 0, // intensity of the hemispheric light. Set to 0 to disable (not really useful if we use an envmap)
+      pointLightIntensity: 0.8,
+      pointLightY: 200,
+      hemiLightIntensity: 0,
     },
-
-    // occluder 3D model:
     GLTFOccluderModel,
-
     bloom: {
-      threshold: 0.5, // apply bloom is light intensity is above this threshold
-      intensity: 8, // intensity of the effect
-      kernelSizeLevel: 0, // 0 -> SMALL kernel
-      computeScale: 0.5, // 0.5 -> compute using half resolution
+      threshold: 0.5,
+      intensity: 8,
+      kernelSizeLevel: 0,
+      computeScale: 0.5,
       luminanceSmoothing: 0.7,
     },
   };
@@ -158,7 +126,6 @@ const VTOGlasses = (props) => {
   let _isPaused = false;
 
   const handle_resize = () => {
-    // do not resize too often:
     if (_timerResize) {
       clearTimeout(_timerResize);
     }
@@ -179,7 +146,6 @@ const VTOGlasses = (props) => {
 
   const toggle_pause = () => {
     if (_isPaused) {
-      // we are in paused state => resume
       mirrorHelper.resume(true);
     } else {
       mirrorHelper.pause(true);
@@ -191,7 +157,6 @@ const VTOGlasses = (props) => {
   const capture_image = () => {
     const threeCanvas = _threeFiber.gl.domElement;
     mirrorHelper.capture_image(threeCanvas).then((cv) => {
-      // download the image in a new window:
       const dataURL = cv.toDataURL('image/png');
       const img = new Image();
       img.src = dataURL;
@@ -203,22 +168,19 @@ const VTOGlasses = (props) => {
   };
 
   useEffect(() => {
-    // init WEBARROCKSFACE through the helper:
     mirrorHelper
       .init({
         NN,
         scanSettings: {
-          threshold: 0.8, // detection threshold, between 0 and 1
+          threshold: 0.8,
         },
         landmarksStabilizerSpec: {
           beta: 10,
           minCutOff: 0.001,
           freqRange: [2, 144],
-          forceFilterNNInputPxRange: [2.5, 6], //[1.5, 4],
+          forceFilterNNInputPxRange: [2.5, 6],
         },
         solvePnPImgPointsLabels: [
-          //'chinLeft', 'chinRight',
-
           'leftEarBottom',
           'rightEarBottom',
           'noseBottom',
@@ -231,10 +193,8 @@ const VTOGlasses = (props) => {
         maxFacesDetected: 1,
       })
       .then(() => {
-        // handle resizing / orientation change:
         window.addEventListener('resize', handle_resize);
         window.addEventListener('orientationchange', handle_resize);
-
         console.log('WEBARROCKSMIRROR helper has been initialized');
       });
 
@@ -244,9 +204,25 @@ const VTOGlasses = (props) => {
     };
   }, [isInitialized]);
 
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setModelChoice(e.target.result);
+      setChoice(true);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  useEffect(() => {
+    if (choice) {
+      setModel(modelChoice);
+      setChoice(false);
+    }
+  }, [modelChoice, choice]);
+
   return (
     <div>
-      {/* Canvas managed by three fiber, for AR: */}
       <Canvas
         className="mirrorX"
         style={{
@@ -255,7 +231,7 @@ const VTOGlasses = (props) => {
           ...sizing,
         }}
         gl={{
-          preserveDrawingBuffer: true, // allow image capture
+          preserveDrawingBuffer: true,
         }}
         updateDefaultCamera={false}
       >
@@ -282,7 +258,6 @@ const VTOGlasses = (props) => {
         </EffectComposer>
       </Canvas>
 
-      {/* Canvas managed by WebAR.rocks, just displaying the video (and used for WebGL computations) */}
       <canvas
         className="mirrorX"
         ref={canvasFaceRef}
@@ -295,14 +270,14 @@ const VTOGlasses = (props) => {
         height={sizing.height}
       />
 
-      {/* <BackButton /> */}
+      <BackButton />
 
       <div className="VTOButtons">
-        <VTOButton onClick={setModel.bind(null, GLTFModel1)}>
-          Glasses 1
-        </VTOButton>
-        <VTOButton onClick={setModel.bind(null, GLTFModel2)}>
-          Glasses 2
+        <VTOButton onClick={() => setModel(GLTFModel1)}>Glasses 1</VTOButton>
+        <VTOButton onClick={() => setModel(GLTFModel2)}>Glasses 2</VTOButton>
+        <VTOButton>
+          {' '}
+          <input type="file" accept=".glb,.gltf" onChange={handleFileChange} />
         </VTOButton>
         <VTOButton ref={togglePauseRef} onClick={toggle_pause}>
           {get_pauseButtonText(_isPaused)}
